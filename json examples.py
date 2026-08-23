89dd7225727700e15e4247636f2795dfe5e9638d
mrprasanna97 <mrprasanna9797@gmail.com>
2026-08-23 18:11:21 +0530

    Delete json examples.py

--- json examples.py
+++
@@ -1,8 +0,0 @@
-import json
-student={"name":"prasanna","age":19,"course":"cse"}
-with open("student.json","w")as f:
-    json.dump(student,f)
-with open("student.json","r")as f:
-    c=json.load(f)
-
-print(c["course"])
